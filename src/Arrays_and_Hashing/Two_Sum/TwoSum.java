1d55e6812d7aa717b202393eac9f0003cdecdd4d
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-08-23 22:17:17 +0530

    "TwoSum Code Solution"

--- src/Arrays_and_Hashing/Two_Sum/TwoSum.java
+++ src/Arrays_and_Hashing/Two_Sum/TwoSum.java
@@ -1,4 +1,18 @@
 package Arrays_and_Hashing.Two_Sum;
 
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int container = target - nums[i];
+
+            if(map.containsKey(container)){
+                return new int[]{map.get(container), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
 }
