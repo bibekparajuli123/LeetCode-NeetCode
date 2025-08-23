a2323e289ba3095fe2e1102b1ed1947796fe6584
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-08-23 22:07:35 +0530

    Optimal using Arrays Collection

--- src/Arrays_and_Hashing/Contains_Duplicate/Optimal.java
+++ src/Arrays_and_Hashing/Contains_Duplicate/Optimal.java
@@ -1,4 +1,20 @@
 package Arrays_and_Hashing.Contains_Duplicate;
 
 public class Optimal {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // duplicate found
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,6};
+        Optimal op = new Optimal();
+        op.hasDuplicate(nums);
+    }
 }
