99f594738be7a0cefb0d9114abef64168a70755c
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-08-24 11:39:25 +0530

    "Valid Palindrome"

--- src/Two_Pointers/Valid_Palindrome/ValidPalindrome.java
+++ src/Two_Pointers/Valid_Palindrome/ValidPalindrome.java
@@ -1,4 +1,24 @@
 package Two_Pointers.Valid_Palindrome;
 
 public class ValidPalindrome {
+    public boolean isPalindrome(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(!Character.isLetterOrDigit(c)){
+                return false;
+            }
+            sb.append(Character.toLowerCase(c));
+        }
+        int n = sb.length();
+
+        int i=0;
+        int j = n-1;
+        while(i<j){
+            if(sb.charAt(i++) != sb.charAt(j--)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
