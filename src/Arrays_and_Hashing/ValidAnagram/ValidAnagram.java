95f924d5d91affdf251f8714d1914a668bdd743d
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-08-24 10:41:16 +0530

    "Valid Anagram"

--- src/Arrays_and_Hashing/ValidAnagram/ValidAnagram.java
+++ src/Arrays_and_Hashing/ValidAnagram/ValidAnagram.java
@@ -1,4 +1,25 @@
 package Arrays_and_Hashing.ValidAnagram;
 
 public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] arr = new int[26];
+
+        //Add al s elements to arr
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a']++;
+        }
+
+        //Subtract s elements using t
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a']--;
+
+            if(arr[s.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return false;
+    }
 }
